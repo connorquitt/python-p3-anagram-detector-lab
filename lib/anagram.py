47bc9d9b27ee2873583e4e6f1637acf8386d5251
connorq <connorquitt@gmail.com>
2024-02-17 14:13:04 -0600

    all tests passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        return[item for item in list if sorted(item) == sorted(self.word)]
